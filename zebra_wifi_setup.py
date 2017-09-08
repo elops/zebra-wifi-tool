ed6818cda96e9b6866b9c1fcebb30c6cee994483
Hrvoje Spoljar <hrvoje.spoljar@gmail.com>
2017-09-08 18:06:06 +0200

    initial code

---
+++ zebra_wifi_setup.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import usb.core
+import os
+import sys
+import binascii
+import traceback
+from passlib.utils import pbkdf2
+
+if os.geteuid() != 0:
+	sys.stderr.write("Pyusb this tool depens on requires root privileges, please run as root. Exiting...")
+	sys.exit(1)
+
+
+VENDOR_ID = 0x0a5f
+PRODUCT_ID = 0x0120
+INTERFACE_ID = 0x0
+SETTING_ID = 0x0
+OUT_ENDPOINT_ID = 0
+IN_ENDPOINT_ID = 1
+
+SSID = raw_input("Enter WiFi SSID : ")
+if not len(SSID):
+	sys.stderr.write("Must provide WiFi SSID. Exiting...")
+	sys.exit(1)
+
+WPA_SECRET = raw_input("Enter WiFi secret : ")
+
+if not len(WPA_SECRET):
+	sys.stderr.write("Must provide WiFi secret. Exiting...")
+	sys.exit(1)
+
+
+# https://github.com/julianofischer/python-wpa-psk-rawkey-gen
+WPA_PSK_KEY = pbkdf2.pbkdf2(str.encode(WPA_SECRET), str.encode(SSID), 4096, 32)
+WPA_PSK_RAWKEY = binascii.hexlify(WPA_PSK_KEY).decode("utf-8").upper()
+
+# Connect string template from Zebra configuration tool for win
+CONNECT_CFG = """
+^XA
+^WIA
+^NC2
+^NPP
+^KC0,0,,
+^WAD,D
+^WEOFF,1,O,H,,,,
+^WP0,0
+^WR,,,,100
+^WS{0},I,L,,,
+^NBS
+^WLOFF,,
+^WKOFF,,,,
+^WX09,{1}
+^XZ
+^XA
+^JUS
+^XZ
+""".format(SSID, WPA_PSK_RAWKEY)
+
+
+device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
+
+if device is None:
+	sys.stderr.write("Could not find Zebra printer attached. Exiting...")
+	sys.exit(1)
+
+
+if device.is_kernel_driver_active(INTERFACE_ID):
+	print "Detaching kernel driver..."
+	device.detach_kernel_driver(INTERFACE_ID) 
+
+try:
+    configuration = device.get_active_configuration()
+except:
+    print "Exception in user code:"
+    print '-'*60
+    traceback.print_exc(file=sys.stdout)
+    print '-'*60
+    sys.exit(1)
+
+# Some ideas and navigation tips from
+# https://gist.github.com/mvidner/04ffe0bbea0fc24182772a196f238918
+interface = configuration[(INTERFACE_ID, SETTING_ID)]
+
+out_endpoint = interface[OUT_ENDPOINT_ID]
+in_endpoint = interface[IN_ENDPOINT_ID]
+
+try:
+	out_endpoint.write(CONNECT_CFG)
+	print("Printer configured with : \n{}".format(CONNECT_CFG))
+except:
+	sys.stderr.write("Error occurred while trying to configure printer, exiting...")
+	sys.exit(1)
+
