78ce5ccd1ae222a4b4e399ef873f5deb1c6ece8c
Hrvoje Spoljar <hrvoje.spoljar@gmail.com>
2017-09-08 22:57:28 +0200

    escape capture.channel1.delimiter properly and add DEVICE_LOCATION conf option

--- zebra_wifi_setup.py
+++ zebra_wifi_setup.py
@@ -39,6 +39,10 @@ if not len(WEBLINK1_URL):
 	sys.stderr.write("Must provide Weblink1 URL. Exiting...\n")
 	sys.exit(1)
 
+DEVICE_LOCATION = raw_input("Enter device location : ")
+if not len(DEVICE_LOCATION):
+	DEVICE_LOCATION = "Default Location"
+
 # https://github.com/julianofischer/python-wpa-psk-rawkey-gen
 WPA_PSK_KEY = pbkdf2.pbkdf2(str.encode(WPA_SECRET), str.encode(SSID), 4096, 32)
 WPA_PSK_RAWKEY = binascii.hexlify(WPA_PSK_KEY).decode("utf-8").upper()
@@ -72,12 +76,14 @@ CONFIG_CMD = """
 ! U1 setvar "ezpl.print_width" "609"
 ! U1 setvar "ezpl.print_method" "direct thermal"
 ! U1 setvar "alerts.configured" "SGD SET,SDK,Y,N,WEBLINK.IP.CONN1,0,N,capture.channel1.data.raw|ALL MESSAGES,SDK,Y,Y,WEBLINK.IP.CONN1,0,N,"
-! U1 setvar "capture.channel1.delimiter" "\\015\\012"
+! U1 setvar "capture.channel1.delimiter" "\\\\015\\\\012"
 ! U1 setvar "capture.channel1.max_length" "10"
 ! U1 setvar "capture.channel1.port" "bt"
 ! U1 setvar "weblink.ip.conn1.location" "{2}"
+! U1 setvar "device.location" "{3}"
+! U1 setvar "device.friendly_name" "ZD420"
 ! U1 setvar "device.reset" "true"
-""".format(SSID, WPA_PSK_RAWKEY, WEBLINK1_URL)
+""".format(SSID, WPA_PSK_RAWKEY, WEBLINK1_URL, DEVICE_LOCATION)
 
 if device.is_kernel_driver_active(INTERFACE_ID):
 	print "Detaching kernel driver..."
